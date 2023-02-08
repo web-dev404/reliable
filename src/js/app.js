0e745262ad1fc3dd45a59ccd8853aa40fd1442c2
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-02-08 16:05:06 +0500

    remove burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import * as flsFunctions from './modules/functions.js';
 import Swiper from 'swiper';
 import { Navigation, EffectFade } from 'swiper';
-import burger from './modules/burger.js';
 import counter from './modules/counter.js';
 import smoothScroll from './modules/smooth-scroll.js';
 import modal from './modules/modal.js';
